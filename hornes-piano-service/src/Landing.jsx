c770408534c8845f65c1f286dce885f6042aaff4
Jared Horne <thomhorne@gmail.com>
2018-05-28 12:28:26 -0500

    added jumbotron theme to landing page

--- hornes-piano-service/src/Landing.jsx
+++ hornes-piano-service/src/Landing.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css'
+import Jumbotron from '../node_modules/react-bootstrap/lib/Jumbotron';
+import Button from '../node_modules/react-bootstrap/lib/Button';
 
 class Landing extends React.Component {
   constructor(props) {
@@ -8,9 +10,15 @@ class Landing extends React.Component {
   }
   render () {
     return (
-      <div className= "jumbotron">
-        <h1>Welcome to Horne's Piano Service</h1>
-      </div>
+      <Jumbotron>
+        <h1>Horne's Piano Service</h1>
+          <p>
+
+          </p>
+          <p>
+            <Button bsStyle="primary">Learn more</Button>
+          </p>
+      </Jumbotron>
 
     )
   }
