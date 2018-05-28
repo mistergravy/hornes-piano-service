7def29262b1b9b9ee537690395ee1c1e334a52de
Jared Horne <thomhorne@gmail.com>
2018-05-28 15:33:56 -0500

    added styling, working on navbar

--- hornes-piano-service/src/Landing.jsx
+++ hornes-piano-service/src/Landing.jsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './App.css'
 import Jumbotron from '../node_modules/react-bootstrap/lib/Jumbotron';
 import Button from '../node_modules/react-bootstrap/lib/Button';
+import Navbar from '../node_modules/react-bootstrap/lib/Navbar';
+import Grid from '../node_modules/react-bootstrap/lib/Grid';
+import Row from '../node_modules/react-bootstrap/lib/Row';
+import Col from '../node_modules/react-bootstrap/lib/Col';
+import Image from '../node_modules/react-bootstrap/lib/Image';
+import Nav from '../node_modules/react-bootstrap/lib/Nav';
+import NavItem from '../node_modules/react-bootstrap/lib/NavItem';
+import NavDropdown from '../node_modules/react-bootstrap/lib/NavDropdown';
+import MenuItem from '../node_modules/react-bootstrap/lib/MenuItem';
 
 class Landing extends React.Component {
   constructor(props) {
@@ -10,14 +19,49 @@ class Landing extends React.Component {
   }
   render () {
     return (
-      <Jumbotron>
-        <h1>Horne's Piano Service</h1>
-          <p>
 
-          </p>
+      <Jumbotron>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">Home</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">
+              About
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Contact
+            </NavItem>
+            <NavDropdown eventKey={3} title="Services" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.4}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
+
+        <h1 class="display-3">Horne's Piano Service</h1>
           <p>
-            <Button bsStyle="primary">Learn more</Button>
+            Welcome to Horne's Piano service! Our site is curently under construction, but you can get in touch with us using one of the methods below.
           </p>
+          <Grid>
+            <Row>
+              <Col xs={6} md={4}>
+              <p>Phone: XXX-XXX-XXXX</p>
+
+              </Col>
+              <Col xs={6} md={4}>
+                Email: hornespianoservice@gmail.com
+              </Col>
+              <Col xs={6} md={4}>
+                Facebook: Horne's Piano Service
+              </Col>
+            </Row>
+          </Grid>
       </Jumbotron>
 
     )
